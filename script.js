60607bdb5be7fd56804f36a5c7a01770eae0fe99
souvikghosh777 <souvikg9474@gmail.com>
2025-12-25 12:44:44 +0530

    random

--- script.js
+++ script.js
@@ -19,15 +19,34 @@ class ExamApp {
         this.bindEvents();
     }
 
+    // Fisher-Yates shuffle algorithm
+    shuffleArray(array) {
+        const shuffled = [...array];
+        for (let i = shuffled.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+        }
+        return shuffled;
+    }
+
     loadQuestions() {
-        // Flatten all questions from pages based on language
-        const langKey = this.language === 'bn' ? 'bn' : 'en';
+        // Shuffle questions within each section, then combine
+        const shuffledPage1 = this.shuffleArray([...examQuestions.page1]);
+        const shuffledPage2 = this.shuffleArray([...examQuestions.page2]);
+        const shuffledPage3 = this.shuffleArray([...examQuestions.page3]);
+        const shuffledPage4 = this.shuffleArray([...examQuestions.page4]);
+        
         this.questions = [
-            ...examQuestions.page1,
-            ...examQuestions.page2,
-            ...examQuestions.page3,
-            ...examQuestions.page4
+            ...shuffledPage1,
+            ...shuffledPage2,
+            ...shuffledPage3,
+            ...shuffledPage4
         ];
+        
+        // Update question IDs to reflect new order (1-100)
+        this.questions.forEach((q, index) => {
+            q.displayId = index + 1;
+        });
     }
 
     initializeElements() {
