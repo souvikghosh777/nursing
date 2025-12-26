ba03bf9791fdb4708ded81a5d893f5657f8116cc
souvikghosh777 <souvikg9474@gmail.com>
2025-12-26 17:29:29 +0530

    performence

--- script.js
+++ script.js
@@ -678,6 +678,12 @@ class ExamApp {
         }, 1000);
     }
 
+    formatTime(seconds) {
+        const mins = Math.floor(seconds / 60);
+        const secs = seconds % 60;
+        return `${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
+    }
+
     submitExam() {
         if (this.timer) {
             clearInterval(this.timer);
